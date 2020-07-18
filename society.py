b100ea322ddcb8b25211de856c122f664330a8e9
Maxwell Henderson <Spider2themax@users.noreply.github.com>
2020-07-18 11:10:38 -0400

    Set up stub class for Society

---
+++ society.py
@@ -0,0 +1,13 @@
+# Copyright MaxAndMitchCorp 2020
+
+
+class Society():
+    """
+    TO DO: Mitchell to code up Society class so that it:
+        (1) Create population_size Person objects
+        (2) Create connections between Person objects, defined by unique Person IDs
+    """
+    def __init__(self,
+                 population_size=25,
+                 connectivity_probability=0.1):
+        pass
