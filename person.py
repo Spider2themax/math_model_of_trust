321e3e03852b90163fdcb15971ba6e3e80de8f35
Maxwell Henderson <Spider2themax@users.noreply.github.com>
2020-07-18 11:06:30 -0400

    Added documentation to Person class

--- person.py
+++ person.py
@@ -6,5 +6,5 @@ import random
 class Person():
 
     def __init__(self):
-        # Initiate person model fields
+        # Initiate person model fields - randomly selects from 0 and 1.
         self._trust = random.random()
