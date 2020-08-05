25af2bf01b2a56d7d54900aa98ecce216d2038f3
mphenderson7777 <32596345+mphenderson7777@users.noreply.github.com>
2020-08-04 21:59:47 -0400

    Update person.py
    Added two sections (one in instantiation and the other in the update_trust method) to ensure that the trust score always remains in the interval [0,1]

--- person.py
+++ person.py
@@ -7,9 +7,19 @@ class Person:
     def __init__(self):
         # Initiate person model fields.
         self._trust = random.random() - random.random()
+        # Make sure that trust score remains in the interval [0,1] 
+        if self._trust < 0:
+            self._trust = 0
+        if self._trust > 1:
+            self._trust = 1
 
     def get_trust(self):
         return self._trust
 
     def update_trust(self, update):
         self._trust = self._trust + update
+        # Make sure that trust score remains in the interval [0,1] 
+        if self._trust < 0:
+            self._trust = 0
+        if self._trust > 1:
+            self._trust = 1
