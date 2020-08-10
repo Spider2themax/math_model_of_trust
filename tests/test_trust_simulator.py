fb49ea6355f782b382983fc3795cc5a579eace18
mphenderson7777 <32596345+mphenderson7777@users.noreply.github.com>
2020-08-10 14:56:53 -0400

    Update test_trust_simulator.py
    Added a test section which tests to make sure that the number of edges in the society decreases when it should

--- tests/test_trust_simulator.py
+++ tests/test_trust_simulator.py
@@ -13,3 +13,37 @@ def test_societal_convergence(alpha):
                                                       iterations=10,
                                                       alpha=alpha)
     assert sum(societal_trust) < 1000
+
+def test_edges_dynamic():
+    # Test to make sure that given a fairly low affiliation probability, that
+    # is, one less than the starting connectivity probability, then the number
+    # of edges decreases over the course of a simulation.
+    
+    # Instantiate the society
+    society = Society(population_size=1000, connectivity_probability=0.3)
+    
+    # Count the number of edges to start
+    count1 = 0
+    for person_id_first in range(society.population_size):
+        for person_id_second in range(society.population_size):
+            if society.edge_matrix[person_id_first][person_id_second]==1:
+                count1 = count1 + 1
+            else:
+                pass
+    # Simulate the society through with an affiliation probability half that
+    # of what it started with.
+    trust_simulator = TrustSimulator()
+    societal_trust = trust_simulator.simulate_society(society=society,
+                                     iterations = 30,
+                                     alpha = 0.1,
+                                     affiliation_prob = 0.15)
+    # Count the edges again with the simulated society.
+    count2 = 0
+    for person_id_first in range(society.population_size):
+        for person_id_second in range(society.population_size):
+            if society.edge_matrix[person_id_first][person_id_second]==1:
+                count2 = count2 + 1
+            else:
+                pass
+    # Assert that count1 should be greater than count2
+    assert count1 > count2
