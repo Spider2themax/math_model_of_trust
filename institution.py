3ff448a6ee09339cdb6b5f7ffdf7255b611428f5
mphenderson7777 <32596345+mphenderson7777@users.noreply.github.com>
2020-08-13 21:37:21 -0400

    Create institution.py
    Ground work for adding the class functioning as the institution.

---
+++ institution.py
@@ -0,0 +1,41 @@
+# Copyright MaxAndMitchCorp 2020
+
+# Create a class to model the institution. It should have a party composition
+# attribute, a method to randomly generate a law to pass (beta distribution)
+# and have its party composition update based off of the affiliations of the
+# society.
+
+from society import Society
+from person import Person
+
+class Institution:
+    
+    def _init_(self, society):
+        self.party_compostion = 0
+        self._calculate_society_comp(society)
+        
+    def _calculate_society_comp(self, society):
+        # Loop through the inputed society and calculate the average party
+        # affiliation, setting party_compostion to this.
+        total_affiliation = 0
+        count = 0
+        for person_id in range(society.population_size):
+            total_affiliation = total_affiliation + society.person_vector[person_id]._party_affiliation
+            count = count + 1
+        
+        if count == 0:
+            self.party_compostion = 0
+        else:
+            self.party_composition = total_affiliation/count
+    
+    def _generate_law(self):
+        # Generate a law (value between -1 and 1) based on a beta distribution.
+        # TO DO
+        pass
+        
+    def _update_party_comp(self, society):
+        # No dynamic aspect to affiliation yet in society, but once there is,
+        # just _calculate_society_comp
+        self._calculate_society_comp(society)
+
+
