c6fce4535e1e2efb3ed05df12bca30831e9448bc
mphenderson7777 <32596345+mphenderson7777@users.noreply.github.com>
2020-08-14 10:19:02 -0400

    Update institution.py
    Added a basic generate law feature, based off of a normal distribution, returning the tanh of the normally generated value

--- institution.py
+++ institution.py
@@ -1,10 +1,12 @@
 # Copyright MaxAndMitchCorp 2020
 
 # Create a class to model the institution. It should have a party composition
-# attribute, a method to randomly generate a law to pass (beta distribution)
+# attribute, a method to randomly generate a law to pass (normal distribution)
 # and have its party composition update based off of the affiliations of the
 # society.
 
+import numpy as np
+import random
 from society import Society
 from person import Person
 
@@ -28,10 +30,12 @@ class Institution:
         else:
             self.party_composition = total_affiliation/count
     
-    def _generate_law(self):
-        # Generate a law (value between -1 and 1) based on a beta distribution.
-        # TO DO
-        pass
+    def _generate_law(self, mean = 0, stdev = 0.5):
+        # Generate a law to be transformed using tanh based on a normal distribution.
+        # Default params: mean = 0, stdev = 0.5
+        # Note: already returns the tanh value
+        law = np.random.normal(loc = mean, scale = stdev)
+        return np.tanh(law)
         
     def _update_party_comp(self, society):
         # No dynamic aspect to affiliation yet in society, but once there is,
