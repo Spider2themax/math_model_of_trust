cf1ab958080b1d0eae6de8846995ec0f2e1aa4a1
mphenderson7777 <32596345+mphenderson7777@users.noreply.github.com>
2020-08-17 15:08:24 -0400

    Update person.py
    Added dynamic fields to person to account for the changing affiliations.

--- person.py
+++ person.py
@@ -9,10 +9,18 @@ class Person:
         self._trust = random.random() - random.random()
         self._party_affiliation = random.random() - random.random()
         self._trust_history = [self._trust]
+        self._affiliation_history = [self._party_affiliation]
 
     def get_trust(self):
         return self._trust
+    
+    def get_affiliation(self):
+        return self._party_affiliation
 
     def update_trust(self, update):
         self._trust = self._trust + update
         self._trust_history.append(self._trust)
+        
+    def update_party_affiliation(self, update):
+        self._party_affiliation = self._party_affiliation + update
+        self._affiliation_history.append(self._party_affiliation)
